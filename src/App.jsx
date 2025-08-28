a7497b21a7740c98e9d62f4ba091c9b10c446723
WallaceFalue <wallaceffalque@gmail.com>
2025-08-28 13:30:07 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTasks";
+
+function App() {
+  return (
+    <div>
+      <h1>Task manager</h1>
+      <AddTask />
+      <AddTask />
+    </div>
+  );
+}
+
+export default App;
