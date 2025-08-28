c3636925fb76e7d376301299ca82f297abf56537
WallaceFalue <wallaceffalque@gmail.com>
2025-08-28 15:55:14 -0300

    Add componets, add frst CSS, change local noraml CSS to TailWind CSS, Firt steps in Task container and Task Creater

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar React",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Ler um livro",
+      description: "Ler 'O Senhor dos Anéis'",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Fazer exercícios",
+      description: "Correr 5km",
+      isCompleted: false,
+    },
+  ]);
+
   return (
-    <div>
-      <h1>Task manager</h1>
-      <AddTask />
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] ">
+        <h1 className="text-3x1 text-slate-100 font-bold text-center">
+          Task manager
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,20 @@
-function Tasks() {
-  return <h1>Tasks</h1>;
+import { ChevronRight, ChevronRightIcon } from "lucide-react";
+function Tasks(props) {
+  console.log(props);
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md mt-6 shadow-md">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button className="bg-slate-400 text-white p-2 rounded-md w-full">
+            {task.title}
+          </button>
+          <button bg-slate-400 p-2 rounded-md text-white>
+            <ChevronRight size={20} />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
