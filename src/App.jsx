6cd648d9fc203f765bd605b69993a320753b0fcc
WallaceFalue <wallaceffalque@gmail.com>
2025-08-29 16:05:35 -0300

    Status task add

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,8 @@ function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] ">
-        <h1 className="text-3x1 text-slate-100 font-bold text-center">
-          Task manager
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Task Managment
         </h1>
         <AddTask />
         <Tasks tasks={tasks} ontaskClick={ontaskClick} />
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,18 +1,19 @@
-import { ChevronRight, ChevronRightIcon } from "lucide-react";
+import { ChevronRight } from "lucide-react";
+
 function Tasks(props) {
-  console.log(props);
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md mt-6 shadow-md">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => props.ontaskClick(task.id)}
-            className="bg-slate-400 text-white p-2 rounded-md w-full"
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.isCompleted ? "line-through" : ""
+            }`}
           >
             {task.title}
-            {task.isCompleted ? " Complet" : " Incompolete"}
           </button>
-          <button bg-slate-400 p-2 rounded-md text-white>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRight size={20} />
           </button>
         </li>
